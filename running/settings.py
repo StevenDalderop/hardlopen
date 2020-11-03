3643fe81a02bbbee9cdb1ff0cd83cb092cab68b7
Steven <stevendalderop@hotmail.com>
2020-11-03 11:50:11 +0100

    changed allowed hosts

--- running/settings.py
+++ running/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [‘0.0.0.0’, ‘localhost’, ‘running-dashboard.herokuapp.com’]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "running-dashboard.herokuapp.com"]
 
 # Application definition
 
