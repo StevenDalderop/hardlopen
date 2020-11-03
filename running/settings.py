bba29904ee58e6a81e7913919544bab7173771f0
Steven <stevendalderop@hotmail.com>
2020-11-03 11:43:54 +0100

    changed static root

--- running/settings.py
+++ running/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [‘0.0.0.0’, ‘localhost’, ‘running-dashboard.herokuapp.com’]
 
 # Application definition
 
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
